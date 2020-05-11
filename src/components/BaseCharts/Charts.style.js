48442d218c913e2e3d38ee729c67cc259fbc72b9
Pablo Navarro Castillo <pnavarrc@gmail.com>
2020-05-11 14:47:14 -0700

    charts: implement the x-axis

--- src/components/BaseCharts/Charts.style.js
+++ src/components/BaseCharts/Charts.style.js
@@ -1,7 +1,28 @@
 import styled from 'styled-components';
 
+// TODO(@pnavarrc): Include the fonts in the app
+const charts = {
+  fontFamily: "'Source Code Pro', 'Roboto', sans-serif",
+  fontWeight: 400,
+  axis: {
+    color: '#666',
+  },
+};
+
 export const ChartContainer = styled.div`
+  /* TODO(@pnavarrc): This negative margin breaks the auto-size of the chart */
   @media (min-width: 996px) {
     margin-left: -3rem;
+  }
+`;
+
+export const Axis = styled.g`
+  text {
+    font-family: ${charts.fontFamily};
+    font-weight: ${charts.fontWeight};
+    fill: ${charts.axis.color};
+  }
+  line {
+    stroke: ${charts.axis.color};
   }
 `;
--- src/components/Charts/ChartRt.tsx
+++ src/components/Charts/ChartRt.tsx
@@ -1,15 +1,86 @@
 import React from 'react';
+import { isUndefined } from 'lodash';
+import { min as d3min } from 'd3-array';
+import { Group } from '@vx/group';
+import { ParentSize } from '@vx/responsive';
+import { scaleTime } from '@vx/scale';
+import { AxisBottom } from '@vx/axis';
 import { ProjectionDataset } from '../../models/Projection';
-import { ChartContainer } from '../BaseCharts';
+import { CHART_END_DATE } from '../../enums/zones';
+import * as Style from '../BaseCharts/Charts.style';
 
 const ChartRt = ({
+  projectionDataset,
+  width,
+  height = 400,
+  marginTop = 5,
+  marginBottom = 40,
+  marginLeft = 40,
+  marginRight = 5,
+}: {
+  projectionDataset: ProjectionDataset;
+  width: number;
+  height?: number;
+  marginTop?: number;
+  marginBottom?: number;
+  marginLeft?: number;
+  marginRight?: number;
+}) => {
+  const chartWidth = width - marginLeft - marginRight;
+  const chartHeight = height - marginTop - marginBottom;
+
+  // Accessing the data
+  const x = (d: any) => new Date(d.x);
+  const yRt = (d: any) => d?.y?.rt;
+  const yHigh = (d: any) => d?.y?.high;
+  const yLow = (d: any) => d?.y?.low;
+
+  const hasData = (d: any) =>
+    !isUndefined(x(d)) &&
+    !isUndefined(yRt(d)) &&
+    !isUndefined(yLow(d)) &&
+    !isUndefined(yHigh(d));
+
+  const data = projectionDataset.data.filter(hasData);
+
+  // TODO(@pnavarrc): This is so TS doesn't complain
+  const minDate = d3min(data, x) || new Date('2020-01-01');
+  const maxDate = CHART_END_DATE;
+
+  const xScale = scaleTime({
+    domain: [minDate, maxDate],
+    range: [0, chartWidth],
+  });
+
+  return (
+    <Style.ChartContainer>
+      <svg width={width} height={height}>
+        <Group left={marginLeft} top={marginTop}>
+          <Style.Axis>
+            <AxisBottom top={chartHeight} scale={xScale} />
+          </Style.Axis>
+        </Group>
+      </svg>
+    </Style.ChartContainer>
+  );
+};
+
+const ChartRtAutosize = ({
   projectionDataset,
   height = 400,
 }: {
   projectionDataset: ProjectionDataset;
   height?: number;
-}) => {
-  return <ChartContainer height={height}></ChartContainer>;
-};
+}) => (
+  <ParentSize>
+    {({ width }) => (
+      <ChartRt
+        width={width}
+        height={height}
+        projectionDataset={projectionDataset}
+      />
+    )}
+  </ParentSize>
+);
 
-export default ChartRt;
+export default ChartRtAutosize;
