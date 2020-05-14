4f1e9d6f3b694d49ca0ed202c1bcb11b15ca74cc
Pablo Navarro Castillo <pnavarrc@gmail.com>
2020-05-13 21:09:31 -0700

    charts: document the boxed annotation component

--- src/components/Charts/BoxedAnnotation.tsx
+++ src/components/Charts/BoxedAnnotation.tsx
@@ -2,6 +2,17 @@ import React, { useRef } from 'react';
 import { Group } from '@vx/group';
 import useSvgBox from './useSvgBox';
 
+/**
+ * This component renders text within a rectangle. In SVG, the size of a text
+ * element is known only at rendering time, so we need to measure and update
+ * the width and height of the rectangle.
+ *
+ * The coordinates (x, y) correspond to the position of the text element in its
+ * SVG context. Note that the alignment of the text relative to (x, y) depends on
+ * the style properties `text-anchor` and `dominant-baseline`, which allows us
+ * to use this component and control alignment with styled components. See
+ * `TextAnnotation` and `RegionAnnotation` in `Charts.style.ts` for examples.
+ */
 export const BoxedAnnotation = ({
   x,
   y,
